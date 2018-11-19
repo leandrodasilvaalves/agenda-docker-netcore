103dc969d6f9ffbbe2ea505d0f2771e9267cdbdf
Leandro da Silva Alves <leandro.silva.alves86@gmail.com>
2018-11-19 20:42:56 -0200

    Carga inicial front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgendaFormComponent } from './agenda-form/agenda-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendaFormComponent
   ],
   imports: [
     BrowserModule,
