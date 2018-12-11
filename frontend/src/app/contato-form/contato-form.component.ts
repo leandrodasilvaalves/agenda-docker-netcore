2fed57d36a46e4555ef38fd78180867e9f3429c9
Leandro da Silva Alves <leandro.alves@s2it.com.br>
2018-12-11 10:50:28 -0200

    Add sweetAlert

--- frontend/src/app/contato-form/contato-form.component.ts
+++ frontend/src/app/contato-form/contato-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IContato } from '../agenda.models';
 import { AgendaService } from '../agenda.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-contato-form',
@@ -35,14 +36,22 @@ export class ContatoFormComponent implements OnInit {
     if(this.idContato == undefined){
       this.servico.incluirContato(this.contato)
       .subscribe(x => {
-        alert("Registro incluso com sucesso.")
+        Swal(
+          'Incluso!',
+          'Registro incluso com sucesso.',
+          'success'
+        );
         this.router.navigate(['']);
       });
     }
     else{
       this.servico.atualizarContato(this.contato)
       .subscribe(x => {
-        alert("Registro atualizado com sucesso.")
+        Swal(
+          'Atualizado!',
+          'Registro atualizado com sucesso.',
+          'success'
+        );
         this.router.navigate(['']);
     });
     }
--- frontend/src/app/contato-lista/contato-lista.component.ts
+++ frontend/src/app/contato-lista/contato-lista.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AgendaService } from '../agenda.service';
 import { IContato } from '../agenda.models';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-contato-lista',
@@ -24,11 +25,26 @@ export class ContatoListaComponent implements OnInit {
   }
 
   excluir(contato: IContato): void {
-    if (confirm(`Deseja excluir o contato: ${contato.nome} ?`)) {
-      this.servico.excluirContato(contato.id).subscribe(x => {
-        alert('Excluído com sucesso');
-        this.contatos.splice(this.contatos.findIndex(c => c == contato),1);
-      });
-    }
+    Swal({
+      title: 'Tem certeza?',
+      text: `Deseja realmente excluir o contato: ${ contato.nome }?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Excluir',
+    }).then((result) => {
+      if (result.value) {
+        this.servico.excluirContato(contato.id).subscribe(x => {
+          this.contatos.splice(this.contatos.findIndex(c => c == contato),1);
+          Swal(
+            'Excluído!',
+            'Contato excluído com sucesso.',
+            'success'
+          );
+        });
+      }
+    });
   }
 }
